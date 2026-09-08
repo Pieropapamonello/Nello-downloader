166d3c5650979345fabde87d43412a9d56490dc8
Pieropapamonello <pieropapamonello@proton.me>
2026-09-08 16:33:29 +0200

    Prefer Facebook SD stream over expensive AV1 sources

--- social_downloader.py
+++ social_downloader.py
@@ -281,7 +281,7 @@ class SocialMediaDownloader(TikTokMixin, InstagramMixin, FacebookMixin, CobaltMi
 
         # Facebook
         if 'facebook' in url.lower() or 'fb.' in url.lower():
-            opts['format'] = 'bestvideo[height<=480]+bestaudio/best[height<=480]/best'
+            opts['format'] = 'sd/bestvideo[height<=720][tbr<=500]+bestaudio/best[height<=720]/best'
             opts['http_headers'].update({
                 'Referer': 'https://www.facebook.com/',
                 'Origin': 'https://www.facebook.com',
